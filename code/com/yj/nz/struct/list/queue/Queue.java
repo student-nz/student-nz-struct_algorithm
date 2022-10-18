10ebfe6fabf9c208481c10ebf92540bceb7e7dec
“itcast” <hello@itcast.cn>
2022-10-18 21:45:00 +0800

    更新数据结构与算法，慢慢吸收

---
+++ code/com/yj/nz/struct/list/queue/Queue.java
@@ -0,0 +1,32 @@
+package com.yj.nz.list.queue;
+
+import com.yj.nz.list.queue.list.LinkedList;
+import com.yj.nz.list.queue.list.List;
+
+public class Queue<E> {
+	private List<E> list = new LinkedList<>();
+	
+	public int size() {
+		return list.size();
+	}
+
+	public boolean isEmpty() {
+		return list.isEmpty();
+	}
+	
+	public void clear() {
+		list.clear();
+	}
+
+	public void enQueue(E element) {
+		list.add(element);
+	}
+
+	public E deQueue() {
+		return list.remove(0);
+	}
+
+	public E front() {
+		return list.get(0);
+	}
+}
